6ca14b646a6158875eac7ba6d3e93f5c50483726
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-10-13 14:40:15 +0200

    Add slide-timer and slide-number & Move slideTimer buttons

--- src/routes/slide/[slideNumber]/+layout.svelte
+++ src/routes/slide/[slideNumber]/+layout.svelte
@@ -27,11 +27,6 @@
     })
 </script>
 
-{$slideTimer}
-<button on:click={slideTimer.reset}>reset</button>
-<button on:click={slideTimer.start}>start</button>
-<button on:click={slideTimer.stop}>stop</button>
-
 <svelte:body
     on:keydown={(event) => {
         const { key } = event
@@ -47,28 +42,60 @@
 
 <div class="height-100p grid-stack">
     <slot />
+    <span id="slide-timer">{$slideTimer}</span>
+    <span id="slide-number">{slideNumber}</span>
     <div id="hover-container">
-        {#if allowPreviousSlide}
-            <a href={previousSlideUrl}>Previous</a>
-        {/if}
-        {#if allowNextSlide}
-            <a href={nextSlideUrl}>Next</a>
-        {/if}
+        <div>
+            <button on:click={slideTimer.reset}>reset</button>
+            <button on:click={slideTimer.start}>start</button>
+            <button on:click={slideTimer.stop}>stop</button>
+        </div>
+        <div>
+            {#if allowPreviousSlide}
+                <a href={previousSlideUrl}>Previous</a>
+            {/if}
+            {#if allowNextSlide}
+                <a href={nextSlideUrl}>Next</a>
+            {/if}
+        </div>
     </div>
 </div>
 
 <style>
     #hover-container {
         place-self: start center;
-        display: flex;
         gap: 1em;
     }
-    #hover-container > a {
+    #hover-container > div {
+        display: flex;
+        gap: 1em;
+        place-content: center;
         scale: 0;
-        display: block;
         transition: scale 0.2s ease-in-out;
     }
-    #hover-container:hover > a {
+    #hover-container:hover > div {
         scale: 1;
+    }
+
+    #slide-timer {
+        place-self: start start;
+        animation: rotate 20s linear infinite reverse;
+    }
+    #slide-number {
+        place-self: start end;
+        animation: rotate 20s linear infinite;
+    }
+    #slide-timer,
+    #slide-number {
+        opacity: 0.3;
+        margin: 1em;
+    }
+    @keyframes rotate {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
     }
 </style>
